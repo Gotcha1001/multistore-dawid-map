f69d346a3ecd71fbcc900f721b5831b73c3351b0
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-26 11:35:55 +0200

    Fixed12

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,19 +1,18 @@
-"use client"
-import { IKContext, IKUpload } from "imagekitio-react";
+import { IKContext, IKUpload, IKUploadProps } from "imagekitio-react";
 
-export default function Uploader(IKUploadProps) {
-    return (
-        <>
-        <IKContext
+export default function Uploader(props: IKUploadProps) {
+  return (
+    <>
+      <IKContext
         urlEndpoint={process.env.NEXT_PUBLIC_IK_ENDPOINT}
         publicKey={process.env.NEXT_PUBLIC_IK_PUBLIC_KEY}
         authenticator={async () => {
-            const response = await fetch('/api/imagekit/auth');
-            return await response.json();
+          const response = await fetch("/api/imagekit/auth");
+          return await response.json();
         }}
-        >
-           <IKUpload {...IKUploadProps} />  
-        </IKContext>
-        </>
-    )
+      >
+        <IKUpload {...props} />
+      </IKContext>
+    </>
+  );
 }
