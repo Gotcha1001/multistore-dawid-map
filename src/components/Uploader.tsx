24e185c1ef952a60963e160096a8275514824183
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-26 12:07:39 +0200

    Fixed15

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,19 +1,19 @@
-import { IKContext, IKUpload } from "imagekitio-react";
+import { IKContext, IKUpload, IKUploadProps } from "imagekitio-react";
 
-// Use Record<string, any> for general props if specific type is unknown
-export default function Uploader(props: Record<string, any>) {
+// Extend IKUploadProps if you need additional custom props
+type UploaderProps = IKUploadProps;
+
+export default function Uploader(props: UploaderProps) {
   return (
-    <>
-      <IKContext
-        urlEndpoint={process.env.NEXT_PUBLIC_IK_ENDPOINT}
-        publicKey={process.env.NEXT_PUBLIC_IK_PUBLIC_KEY}
-        authenticator={async () => {
-          const response = await fetch("/api/imagekit/auth");
-          return await response.json();
-        }}
-      >
-        <IKUpload {...props} />
-      </IKContext>
-    </>
+    <IKContext
+      urlEndpoint={process.env.NEXT_PUBLIC_IK_ENDPOINT || ""}
+      publicKey={process.env.NEXT_PUBLIC_IK_PUBLIC_KEY || ""}
+      authenticator={async () => {
+        const response = await fetch("/api/imagekit/auth");
+        return await response.json();
+      }}
+    >
+      <IKUpload {...props} />
+    </IKContext>
   );
 }
