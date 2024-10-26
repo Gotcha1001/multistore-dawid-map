b8d644f37641e8e7024005b598291d207ccb7d97
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-26 17:24:50 +0200

    Master

--- src/components/AdItem.tsx
+++ src/components/AdItem.tsx
@@ -1,11 +1,10 @@
 "use client";
-
+import { Ad } from "@/models/Ad";
 import Link from "next/link";
 import UploadThumbnail from "./UploadThumbnail";
 import { motion } from "framer-motion";
 
-// Update props to use the correct type for ad
-export default function AdItem({ ad }: { ad: ConvertedAd }) {
+export default function AdItem({ ad }: { ad: Ad }) {
   return (
     <div className="min-h-24 flex flex-col justify-start" key={ad._id}>
       {ad.files?.length > 0 && (
