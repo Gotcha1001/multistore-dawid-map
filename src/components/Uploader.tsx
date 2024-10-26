49fce11e9896bbafa7d7cbb4cf7a77d685010db9
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-26 12:23:16 +0200

    Li

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,43 +1,7 @@
 import { IKContext, IKUpload } from "imagekitio-react";
-import { UploadResponse } from "imagekit/dist/libs/interfaces";
 
-// Define upload progress event type
-interface IKUploadProgressEvent {
-  loaded: number;
-  total: number;
-  percentage: number;
-}
-
-// Define upload start event type
-interface IKUploadStartEvent {
-  file: File;
-  xhr: XMLHttpRequest;
-}
-
-interface UploaderProps {
-  fileName?: string;
-  tags?: string[];
-  useUniqueFileName?: boolean;
-  responseFields?: string[];
-  validateFile?: (file: File) => boolean;
-  folder?: string;
-  isPrivateFile?: boolean;
-  customCoordinates?: string;
-  extensions?: string[];
-  webhookUrl?: string;
-  overwriteFile?: boolean;
-  overwriteAITags?: boolean;
-  overwriteTags?: boolean;
-  overwriteCustomMetadata?: boolean;
-  transformation?: string;
-  customMetadata?: Record<string, unknown>;
-  onError?: (err: Error) => void;
-  onSuccess?: (response: UploadResponse) => void;
-  onUploadStart?: (evt: IKUploadStartEvent) => void;
-  onUploadProgress?: (evt: IKUploadProgressEvent) => void;
-  style?: React.CSSProperties;
-  className?: string;
-}
+// Extend IKUploadProps if you need additional custom props
+type UploaderProps = IKUploadProps;
 
 export default function Uploader(props: UploaderProps) {
   return (
