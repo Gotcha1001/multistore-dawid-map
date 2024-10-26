add6086b8116da4a32f882161137da21e4313e5a
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-26 11:19:42 +0200

    Fixed7

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,16 +1,23 @@
 import { ReactNode } from "react";
 import { useFormStatus } from "react-dom";
 
-export default function SubmitButton({ children }: { children: ReactNode }) {
+type SubmitButtonProps = {
+  children: ReactNode;
+  className?: string; // Add className as an optional prop
+};
+
+export default function SubmitButton({
+  children,
+  className,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
     <button
       disabled={pending}
-      className={
-        (pending ? "bg-gray-400" : "bg-blue-600") +
-        " mt-3 text-white px-6 py-2 rounded-lg"
-      }
+      className={`${
+        pending ? "bg-gray-400" : "bg-blue-600"
+      } ${className} mt-3 text-white px-6 py-2 rounded-lg`}
     >
       {pending ? <span>Saving...</span> : <span>{children}</span>}
     </button>
