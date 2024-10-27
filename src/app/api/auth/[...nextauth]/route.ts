4f08c085872b522914a67cce3f272fe96f52c71e
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-27 11:31:32 +0200

    FInally

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,7 @@
-import { getServerSession } from "next-auth";
-
-import ImageKit from "imagekit";
+import NextAuth from "next-auth";
 import { authOptions } from "./auth";
 
-export const GET = async () => {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return Response.json(false);
-  }
+const handler = NextAuth(authOptions);
 
-  const ik = new ImageKit({
-    urlEndpoint: process.env.NEXT_PUBLIC_IK_ENDPOINT as string,
-    publicKey: process.env.NEXT_PUBLIC_IK_PUBLIC_KEY as string,
-    privateKey: process.env.IK_PRIVATE_KEY as string,
-  });
-  return Response.json(ik.getAuthenticationParameters());
-};
+// Export handlers for GET and POST requests
+export { handler as GET, handler as POST };
